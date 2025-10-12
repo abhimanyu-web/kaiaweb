42731b377c6b242b993fb903e6bb3a8304e5aa0e
Abhimanu Jha <abhimanyujha2015@gmail.com>
2025-10-13 01:49:35 +0530

    changed order again due to positioning

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -172,8 +172,8 @@ export default function Hero() {
           playsInline
           className="h-[150px] sm:h-[200px] md:h-[300px] w-auto object-contain mx-auto"
         >
-          <source src={logoVideoMov} type='video/mp4; codecs="hvc1"' />
           <source src={logoVideoWebm} type="video/webm" />
+          <source src={logoVideoMov} type='video/mp4; codecs="hvc1"' />
 
           Your browser does not support the video tag.
         </video>
