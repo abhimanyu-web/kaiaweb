aa8b90417687f71cf8f7498c53799f8425be2e6f
Abhimanyu-web <abhimanyu@vibezestates.com>
2025-11-05 14:32:07 +0530

    back to Index

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,10 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.jsx';
-import TagManager from 'react-gtm-module';
-
-// Initialize GTM
-const tagManagerArgs = {
-  gtmId: 'GTM-WHQTQMNQ', // replace with your GTM ID
-};
-
-TagManager.initialize(tagManagerArgs);
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>
-);
+  </StrictMode>,
+)
